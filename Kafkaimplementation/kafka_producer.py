819c4197498e92aa072ad9b37ddacccdc80ad818
Shikhar Mishra <144837855+ShikharMishra16@users.noreply.github.com>
2025-11-28 13:09:29 +0530

    Update kafka_producer.py

--- Kafkaimplementation/kafka_producer.py
+++ Kafkaimplementation/kafka_producer.py
@@ -4,7 +4,7 @@ import pandas as pd
 import time
 import json
 
-# ✅ Load the correct test dataset
+# Load the correct test dataset
 df = pd.read_csv("test_frauds2.csv")
 
 producer = KafkaProducer(
@@ -14,8 +14,8 @@ producer = KafkaProducer(
 
 for _, row in df.iterrows():
     producer.send("transactions", value=row.to_dict())
-    print("📤 Sent:", row.to_dict())
+    print("Sent:", row.to_dict())
     time.sleep(1)
 
-print("✅ All test frauds sent.")
+print("All test frauds sent.")
 
